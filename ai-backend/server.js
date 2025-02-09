7423de09b399a39a067019b408f0578082f7e789
DreamerAmit <amitmahajan274@outlook.com>
2025-02-09 21:51:34 +0530

    pic2alt.com cors

--- ai-backend/server.js
+++ ai-backend/server.js
@@ -25,6 +25,7 @@ const allowedOrigins = [
   'http://pic2alt.com',
   'https://pic2alt.com',
   'https://www.pic2alt.com',
+  'http://www.pic2alt.com',
   'https://bc93-2405-204-901b-aaa3-ca6-7ce5-9b2c-196a.ngrok-free.app'
 ];
 
